0a88ed9ba807c63e4826c85ffec2fd73a479921b
mai Essam <maiEssam75@yahoo.com>
2019-06-28 22:40:26 +0200

    first commit

--- footer.php
+++ footer.php
@@ -18,18 +18,12 @@
             <div class="sk-cube sk-cube9"></div>
         </div>
     </div> 
-     <script type="text/javascript" src="assets/js/main.js"></script>
-    <script src="assets/js/jquery.js" type=''></script>
+   
     <script src="assets/js/jquery-1.11.1.min.js" type=''></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
-
-
     <script src="assets/js/jquery.countTo.js" type=''></script>
-    <script src="assets/js/main.js" type=''></script>
+    <script src="assets/js/main.js" type='text/javascript'></script>
   </body>
 </html>
