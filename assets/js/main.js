2bc9fb0b081f0922f7a1a8f6fc7f770997bcbc8f
MaiEssam75 <mai.essam75@yahoo.com>
2019-06-30 19:17:30 +0200

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,43 @@
      });
  
  });
- function validateform(){  
-    var x=document.form["signupform"]["usernameBox"]["passwordBox"].value;   
-      
-    if (x==null || x==""||x=='undefined'||x.length<=3){  
-      alert("Name can't be blank");  
-      return false;  
-    }else if(password.length<6){  
-      alert("Password must be at least 6 characters long.");  
-      return false;  
-      }  
-    }  
+
+ var form = document.getElementById('validateform');
+ var allFormInput = form.elements; 
+ allFormInput[0].addEventListener("input",validateform)
+ function validateform(){
+  if(this.value == "" || this.value.indexOf("<") !== -1 || this.value.indexOf(">") !== -1 ||typeof(this.value) == null || typeof(this.value) == 'undefined'  || this.value.length <= 5 || this.value.length >= 20){
+    document.getElementById('error').innerHTML = '<h1>Error Username </h1>';
+    return false;
+    }
+    else if(this.value.search(/\s/g) > -1)
+    {
+      document.getElementById('error').innerHTML = '<h1>Error whitespace </h1>';
+    return false;
+    }
+    else {
+    document.getElementById('error').innerHTML = '<h1 style=\"color:green\;\">Correct Username </h1>';
+    }
+    };
+  
+   
+    var y=document.form["passwordBox"]["cpasswordBox"].value;   
+    allFormInput[1].addEventListener("input",matchpass)
+    function matchpass(){  
+    // if (y==null || y==""||y=='undefined'||y.length<=3){  
+    //   alert("password can't be blank");  
+    //   return false;  
+    // }else if(password.length<6){  
+    //   alert("Password must be at least 6 characters long.");  
+    //   return false;  
+    //   } 
+     
+      if(passwordBox==cpasswordBox){  
+        return true;  
+        }  
+        else{  
+        alert("password must be same!");  
+        return false;  
+        }    
+    };
  
--- signup.php
+++ signup.php
@@ -1,17 +1,18 @@
 <?php require "header.php";?>
 <main>
       <section class="signupform">
-            <form action="signup.inc.php" method="post" name="signupform">
+            <form action="signup.inc.php" method="post" id="validateform" name="signupform"onsubmit="return validateform()">
                   <h1 style="color:green;border-bottom:1px solid #f1f0ea;padding-bottom:20px;text-align: center">Create a New Account</h1><br/>
+                  <p id='error'></p>
                  <h4> User Name : </h4>
                   <input type="text" name="usernameBox" onsubmit="return validateform()" placeholder="Your User Name" required/><br/>
                   <h4> Email : </h4>
-                  <input type="email" name="emailBox" onsubmit="return validateform()" placeholder="Your Email" require/><br/>
+                  <input type="email" name="emailBox" onsubmit="" placeholder="Your Email" required/><br/>
                   <h4> password :  </h4>
-                  <input type="password" name="passwordBox"onsubmit="return validateform()" placeholder="Password"require /><br/>
+                  <input type="password" name="passwordBox"onsubmit="return matchpass()" placeholder="Password" required /><br/>
                   <h4> confirm password : </h4>
-                  <input type="password" name="cpasswordBox"onsubmit="return validateform()" placeholder="Confirm Password" require/><br/><br/><br/>
-                  <button type="submit" class="btn"style="margin-left: 50px;width: 400px;margin-bottom: 30px;" name="signupSubmit">Sign up</button>
+                  <input type="password" name="cpasswordBox"onsubmit="return matchpass()" placeholder="Confirm Password" required/><br/><br/><br/>
+                  <button type="submit" class="btn"style="margin-left: 50px;width: 400px;margin-bottom: 30Spx;" name="signupSubmit">Sign up</button>
             </form>
       </section>
 </main>
